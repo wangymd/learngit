8808e4505b60187bcc39646f88d794c9ce301cb4
wangymd <314851768@qq.com>
2017-04-23 23:16:19 +0800

    GitTest2

---
+++ GitTest1/src/com/git/test1/GitTest2.java
@@ -0,0 +1,14 @@
+package com.git.test1;
+/**
+ * Git测试
+ * @author wangymd
+ *
+ */
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
